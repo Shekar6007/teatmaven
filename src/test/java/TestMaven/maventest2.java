ccea26f4ce1d2e925d4113345da9ff7cf4c7fd27
User <User@DESKTOP-SHH2K20>
2024-03-03 10:07:53 +0530

    this is first commit

--- src/test/java/TestMaven/maventest2.java
+++ src/test/java/TestMaven/maventest2.java
@@ -1,5 +1,20 @@
 package TestMaven;
 
+import org.testng.annotations.Test;
+
 public class maventest2 {
+
+	
+	@Test
+	public void test4(){
+		
+		System.out.println("test 4 is printed");
+	}
+	
+	@Test
+public void test5(){
+		
+		System.out.println("test 5 is printed");
+	}
 
 }
